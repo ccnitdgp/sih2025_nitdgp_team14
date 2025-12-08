abff2510aa219f94be70dc055f8161b14ab4ffa9
SWASTHYA ! <code4curefinal@gmail.com>
2025-12-08 20:13:02 +0000

    in doctors panel upload documents names are not visible in select patien

--- src/app/doctor-dashboard/upload-documents/page.tsx
+++ src/app/doctor-dashboard/upload-documents/page.tsx
@@ -5,7 +5,7 @@ import { useState, useMemo, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
-import { useUser, useFirestore, useCollection, useMemoFirebase, useFirebaseApp, setDocumentNonBlocking, updateDocumentNonBlocking } from '@/firebase';
+import { useUser, useFirestore, useCollection, useMemoFirebase, useFirebaseApp, updateDocumentNonBlocking } from '@/firebase';
 import { collection, serverTimestamp, doc, addDoc, query, where } from 'firebase/firestore';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { useToast } from '@/hooks/use-toast';
